94f9b177a07caa669df50c31848fa6fff06627c2
Shubham Tiwari <shubhamtiwari06112004@gmail.com>
2024-09-03 01:22:26 +0530

    fixed organization join issue

--- lib/views/auth/onboarding.dart
+++ lib/views/auth/onboarding.dart
@@ -17,19 +17,25 @@ class _UserOnbaordingPageState extends State<UserOnbaordingPage> {
   bool isLoading = false;
   Map<int, String> orgInvites = {};
 
+  int joinLoading = -1;
+
   Future<void> getOrgInvites() async {
     final data = await supabase
-        .from('organization_invites')
+        .from('organization_invitations')
         .select("organization_id, organizations(name)");
     Map<int, String> invites = {};
     for (var element in data) {
       invites[element['organization_id']] = element['organizations']['name'];
     }
+    setState(() {
+      orgInvites.clear();
+      orgInvites.addAll(invites);
+    });
   }
 
   void joinOrg(int id) async {
     setState(() {
-      isLoading = true;
+      joinLoading = id;
     });
     try {
       await supabase.from('organization_members').insert({
@@ -56,7 +62,7 @@ class _UserOnbaordingPageState extends State<UserOnbaordingPage> {
       }
     }
     setState(() {
-      isLoading = false;
+      joinLoading = -1;
     });
   }
 
@@ -105,11 +111,12 @@ class _UserOnbaordingPageState extends State<UserOnbaordingPage> {
 
   @override
   void initState() {
-    super.initState();
     Session? session = supabase.auth.currentSession;
     if (session == null) {
       Navigator.of(context).restorablePushReplacementNamed('/login');
     }
+    getOrgInvites();
+    super.initState();
   }
 
   @override
@@ -143,30 +150,48 @@ class _UserOnbaordingPageState extends State<UserOnbaordingPage> {
                         "Welcome to e-Invoice!",
                         style: ShadTheme.of(context).textTheme.h2,
                       ),
+                      const SizedBox(height: defaultPadding),
                       if (orgInvites.isNotEmpty) ...[
-                        ...orgInvites.entries.map((e) {
-                          return Container(
-                            padding: const EdgeInsets.all(4.0),
-                            decoration: BoxDecoration(
-                              color: ShadTheme.of(context).colorScheme.border,
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Row(
-                              children: [
-                                Text(
-                                  e.value,
-                                  style: ShadTheme.of(context).textTheme.p,
+                        ...orgInvites.entries.map(
+                          (e) {
+                            return Card(
+                              margin: const EdgeInsets.all(2),
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  width: 2.0,
                                 ),
-                                ShadButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pushNamed('/join');
-                                  },
+                              ),
+                              child: ListTile(
+                                leading: const Icon(
+                                  LucideIcons.globe,
+                                ),
+                                title: Text(
+                                  e.value,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: false,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                                trailing: ShadButton(
+                                  icon: joinLoading == e.key
+                                      ? const SizedBox.square(
+                                          dimension: 16,
+                                          child: CircularProgressIndicator(
+                                              strokeWidth: 2),
+                                        )
+                                      : null,
+                                  onPressed: () => joinOrg(e.key),
                                   child: const Text('Join'),
                                 ),
-                              ],
-                            ),
-                          );
-                        }),
+                              ),
+                            );
+                          },
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
