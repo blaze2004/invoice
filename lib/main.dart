c662a2dbfe8151d657787a885cfbea187aca998f
Shubham Tiwari <shubhamtiwari06112004@gmail.com>
2024-09-02 20:32:19 +0530

    fixe denv issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:invoice/splash_page.dart';
 import 'package:invoice/views/auth/onboarding.dart';
@@ -9,17 +7,14 @@ import 'package:invoice/views/dashboard/main.dart';
 import 'package:invoice/views/invoice/new.dart';
 import 'package:invoice/views/templates/main.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 Future<void> main() async {
   usePathUrlStrategy();
-  await dotenv.load(
-      fileName: kIsWeb ? 'env': ".env");
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: const String.fromEnvironment("SUPABASE_URL"),
+    anonKey: const String.fromEnvironment("SUPABASE_ANON_KEY"),
   );
   runApp(const InvoiceApp());
 }
